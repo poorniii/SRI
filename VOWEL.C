458baefd0923c38c3d69c6bb9758300aa5f57ccd
poorniii <35651828+poorniii@users.noreply.github.com>
2018-01-21 16:37:02 +0530

    VOWEL.C
    check whether a character is vowel or constant

---
+++ VOWEL.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int lowercase,uppercase;
+char c;
+printf("enter an alphabet:");
+scanf("%d",&c);
+printf("enter the lowercasevowels");
+scanf("%d",&l);
+printf("enter the uppercasevowels");
+scanf("%d",&u);
+return 0;
+}
